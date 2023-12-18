6e1ae934ce21ba141b747aea66e5feefbea75f77
MMDavito <davidking9753@gmail.com>
2023-12-18 18:53:51 +0100

    Speeded up reading

--- SerialIntToHexaDecimal.ino
+++ SerialIntToHexaDecimal.ino
@@ -11,43 +11,43 @@ const byte reset = 0xFF;
 
 //MostSignificant, to LSB
 //First 3 is decimal, last two is Hex
-byte digitPins[] = {11,12,13,9,10};
+byte digitPins[] = {11, 12, 13, 9, 10};
 
 SevenSegmentDisplay screenName(2, 8, 7, 6, 5, 3, 4, NULL, false);//CommonCathode no decimal-point
 
 
 const unsigned int MAX_MESSAGE_LENGTH = 12;
-char decimalArray [3] = {'0','0','0'};
+char decimalArray [3] = {'0', '0', '0'};
 String decimal = "";
 String hex = "";
-char hexArray[2] = {'0','0'};
+char hexArray[2] = {'0', '0'};
 
 void byteToArrays(byte value) {
-  decimal="";
-  hex="";
-  
-  
-  if(value < 0x10) hex+= "0";
-  if(value < 100) decimal += "0";
-  if(value < 10) decimal += "0";
+  decimal = "";
+  hex = "";
 
-  hex += String(value,HEX);
+
+  if (value < 0x10) hex += "0";
+  if (value < 100) decimal += "0";
+  if (value < 10) decimal += "0";
+
+  hex += String(value, HEX);
   hex.toUpperCase();
   /*
-  Serial.print("HEX: ");
-  Serial.println(hex);
-  Serial.println("");
+    Serial.print("HEX: ");
+    Serial.println(hex);
+    Serial.println("");
   */
   decimal += String(value, DEC);
   /*
-  Serial.print("Decimal: ");
-  Serial.println(decimal);
-  Serial.print("length: ");
-  Serial.println(decimal.length());
+    Serial.print("Decimal: ");
+    Serial.println(decimal);
+    Serial.print("length: ");
+    Serial.println(decimal.length());
   */
   decimal.toCharArray(decimalArray, 4);
-  if(value >=100 && value < 200) decimalArray[0] = '1';
-  else if(value >= 200) decimalArray[0] = '2';
+  if (value >= 100 && value < 200) decimalArray[0] = '1';
+  else if (value >= 200) decimalArray[0] = '2';
   else decimalArray[0] = '0';
 
   //Serial.println("DecimalArray:");
@@ -56,60 +56,65 @@ void byteToArrays(byte value) {
   //Serial.println("");
   hex.toCharArray(hexArray, 3);
   /*
-  Serial.println("HexArray:");
-  for(int i=0; i<2; i++) Serial.println(hexArray[i]);
+    Serial.println("HexArray:");
+    for(int i=0; i<2; i++) Serial.println(hexArray[i]);
   */
-  
+
 }
 
 
 void setup() {
- Serial.begin(9600);
- 
-  for(int i=0; i<5; i++)
+  Serial.begin(9600);
+
+  for (int i = 0; i < 5; i++)
   {
-    pinMode(digitPins[i],OUTPUT);
-    digitalWrite(digitPins[i],LOW);
+    pinMode(digitPins[i], OUTPUT);
+    digitalWrite(digitPins[i], LOW);
   }
- 
+
+}
+
+byte readSerial() {
+  delay(5);
+  String recieved = "";
+  int numChars = 0;
+  while (Serial.available()) {
+    byte b = Serial.read() - '0';
+    if (b == 218) break;
+    recieved += b;
+    numChars ++;
+    //if(numChars == 3) return recieved.toInt();
+  }
+  return recieved.toInt();
 }
 
 void loop() {
   //Check to see if anything is available in the serial receive buffer
   int numChars = 0;
-  while (Serial.available() > 0)
+  if (Serial.available())
   {
-    //Create a place to hold the incoming message
-    byte inByte = Serial.parseInt();
-    if(inByte == NULL && numChars > 0) continue;
-    numChars += 1;
-    /*
-    Serial.print("inByte:");
-    Serial.println(inByte);
-    Serial.print("b_in;");
-    Serial.println(b_in);
-    Serial.println("");
-    */
+
+    byte inByte = readSerial();
     byteToArrays(inByte);
   }
-  for(int i=0; i<5; i++)
+  for (int i = 0; i < 5; i++)
   {
-    if(i>2)
+    if (i > 2)
     {
       screenName.displayCharacter(hexArray[i - 3]);
-      digitalWrite(digitPins[i],HIGH);
+      digitalWrite(digitPins[i], HIGH);
       long start = micros();
-      while((micros() - start) < 500)//do nothing
-        if((micros() - start)>10000) break;
+      while ((micros() - start) < 500) //do nothing
+        if ((micros() - start) > 10000) break;
     }
     else
     {
       screenName.displayCharacter(decimalArray[i]);
-      digitalWrite(digitPins[i],HIGH);
+      digitalWrite(digitPins[i], HIGH);
       long start = micros();
-      while((micros() - start) < 100)//do nothing
-        if((micros() - start)>10000) break;
+      while ((micros() - start) < 100) //do nothing
+        if ((micros() - start) > 10000) break;
     }
-    digitalWrite(digitPins[i],LOW); 
+    digitalWrite(digitPins[i], LOW);
   }
 }
